5a193fe1ec38d9b1a527eec2c0a3216436d2710e
Pedro Matheus <matt_4.6.8@outlook.com>
2022-09-27 21:26:44 -0300

    Feito Tests com o banco buscando uma doacao e inserindo uma doacao

---
+++ AjudaFacil/AjudaFacil/Controllers/DonationsController.cs
@@ -0,0 +1,36 @@
+﻿using AjudaFacil.Data;
+using AjudaFacil.Models;
+using AjudaFacil.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjudaFacil.Controllers
+{
+    public class DonationsController : Controller
+    {
+        // retorna a primeira doacao de material escolar que encontrar
+        [HttpGet, ActionName("GetAllSchoolSupplieDonation")]
+        public IActionResult GetAllSchoolSupplieDonation([FromServices] DataContext context)
+        {
+            var result = context.SchoolSupplieDonation.FirstOrDefault();
+            return View(result);
+        }
+
+        public IActionResult CreateSchoolSupplieDonation()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("CreateSchoolSupplieDonation")]
+        public IActionResult CreateSchoolSupplieDonation([FromServices] DataContext context, SchoolSupplieDonation schoolSupplieDonation)
+        {
+            var donation = context.Donation.FirstOrDefault(x => x.Id == 2);
+            schoolSupplieDonation.Donations = donation;
+
+            SchoolSupplieDonationService donationService = new SchoolSupplieDonationService();
+            donationService.InsertAsync(context, schoolSupplieDonation);
+
+
+            return RedirectToAction(nameof(CreateSchoolSupplieDonation));
+        }
+    }
+}
---
+++ AjudaFacil/AjudaFacil/Services/SchoolSupplieDonationService.cs
@@ -0,0 +1,14 @@
+﻿using AjudaFacil.Data;
+using AjudaFacil.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjudaFacil.Services;
+
+public class SchoolSupplieDonationService
+{
+    public async Task InsertAsync([FromServices] DataContext context,SchoolSupplieDonation donation)
+    {
+        context.Add(donation);
+        await context.SaveChangesAsync();
+    }
+}
