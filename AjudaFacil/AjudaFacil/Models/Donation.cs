88c516c1cc6769023afac138f1a2424bcc0f641d
Pedro Matheus <matt_4.6.8@outlook.com>
2022-10-01 16:21:21 -0300

    Iniciado a segunda vercao do projeto AjudaFacil

--- AjudaFacil/AjudaFacil/Models/Donation.cs
+++ AjudaFacil/AjudaFacil/Models/Donation.cs
@@ -4,6 +4,9 @@ public class Donation
 {
     public int Id { get; set; }
     public User User { get; set; }
-    public IList<SchoolSupplieDonation> SchoolSupplieDonations { get; set; }
+    public IList<SchoolSupplieDonation> SchoolSupplieDonations { get; set; } 
     public IList<ClothingDonation> ClothingDonations { get; set; }
 }
+
+// fazer no proximo model que o usuario tem uma donation e uma donation n tem muitas
+// donations de um tipo e sim 1 donation de alimentos ou de outro tipo
