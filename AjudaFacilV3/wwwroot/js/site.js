66ee67191b8616777e60d8986d96cd632cbb236f
Pedro Matheus <matt_4.6.8@outlook.com>
2022-10-11 10:27:18 -0300

    Adicionado comando para por foto de perfil

--- AjudaFacilV3/wwwroot/js/site.js
+++ AjudaFacilV3/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var files = document.querySelector('input[name="files"]');
 
-// Write your JavaScript code.
+files.addEventListener("change", function (file) {
+    var input = file.target;
+
+    var reader = new FileReader();
+
+    reader.onload = function () {
+        var dataURL = reader.result;
+        var output = document.getElementById('output');
+        output.src = dataURL;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+});
+
+
+// PREVIEW FOTO
+/*function PreviewImage() {
+    var oFReader = new FileReader();
+oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+
+oFReader.onload = function (oFREvent) {
+    document.getElementById("uploadPreview").src = oFREvent.target.result;
+    };
+};*/
